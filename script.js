ab7460d03387eff549e3b36eb1dd0a7077ed3b75
Arjun singh gurjar <115486632+Arjun09091999@users.noreply.github.com>
2023-06-22 12:25:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+const gameContainer = document.getElementById('gameContainer');
+const scoreSpan = document.getElementById('score');
+
+// Generate pixels
+for (let i = 0; i < 40; i++) {
+  for (let j = 0; j < 40; j++) {
+    const pixel = document.createElement('div');
+    pixel.id = `pixel${i * 40 + j + 1}`;
+    pixel.classList.add('pixel');
+    gameContainer.appendChild(pixel);
+  }
+}
